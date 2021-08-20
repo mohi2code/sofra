94fc90859c42e189a32f00981bc2064ea66d5aa8
mohi2code <mohyaldeen0101ahmed@gmail.com>
2021-08-21 02:54:28 +0300

    :spoon: some animations

--- sections/About.js
+++ sections/About.js
@@ -1,22 +1,53 @@
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Container from '../components/Container'
 import { PrimaryButton } from '../components/Button'
+import { motion } from 'framer-motion'
+import VisibilitySensor from 'react-visibility-sensor'
 import styles from './About.module.css'
 
 export default function About() {
 
+    const [animated, setAnimated] = useState(false)
+
     const router = useRouter()
 
     return (
-        <Container id={styles.about}>
-            <div className={styles.image}></div>
-            <div className={styles.text}>
-                <h3>About</h3>
-                <h1>Who are we ?</h1>
-                <p>We Believe the key to our success is through our customers thats why quality and satisfaction is our daily strive; looking forward to have you as our guest.</p>
-                <strong><p>established since 1970</p></strong>
-                <PrimaryButton onClick={()=>router.push("/about")}>Learn more</PrimaryButton>
-            </div>
-        </Container>
+        <VisibilitySensor onChange={isVisible => isVisible&&setAnimated(true) }>
+            {({isVisible}) =>
+                <Container id={styles.about}>
+                    <motion.div 
+                        variants={variants1}
+                        initial="initial"
+                        animate={ animated ? "animate": "initial"}
+                        transition={{ stiffness: 1000 }}
+                        className={styles.image}
+                    ></motion.div>
+                    <motion.div 
+                        variants={variants2}
+                        initial="initial"
+                        animate={ animated ? "animate": "initial"}
+                        transition={{ stiffness: 1000 }}
+                        className={styles.text}
+                    >
+                        <h3>About</h3>
+                        <h1>Who are we ?</h1>
+                        <p>We Believe the key to our success is through our customers thats why quality and satisfaction is our daily strive; looking forward to have you as our guest.</p>
+                        <strong><p>established since 1970</p></strong>
+                        <PrimaryButton onClick={()=>router.push("/about")}>Learn more</PrimaryButton>
+                    </motion.div>
+                </Container>
+            }
+        </VisibilitySensor>
     )
+}
+
+const variants1 = {
+    initial: { opacity: 0, transform: "translateX(-10px)" },
+    animate: { opacity: 1, transform: "translateX(0px)" },
+}
+
+const variants2 = {
+    initial: { opacity: 0, transform: "translateX(10px)" },
+    animate: { opacity: 1, transform: "translateX(0px)" }
 }
--- sections/Hero.js
+++ sections/Hero.js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import Container from '../components/Container'
 import NavbarSection from './NavbarSection'
 import { PrimaryButton } from '../components/Button'
+import { motion } from 'framer-motion'
 import styles from './Hero.module.css'
 
 export default function Hero() {
@@ -12,17 +13,28 @@ export default function Hero() {
         <Container id={styles.hero}>
             <NavbarSection />
 
-            <div className={styles.showcase}>
-                <div className={styles.text}>
+            <motion.div 
+                variants={variants}
+                initial="initial"
+                animate="animate"
+                transition={{ stiffness: 10000, delayChildren: .1, staggerChildren: .25 }}
+                className={styles.showcase}
+            >
+                <motion.div className={styles.text} variants={variants}>
                     <h1>Where taste meets the myth</h1>
                     <p>We are looking forward to having you as our guest</p>
                     <PrimaryButton onClick={() => router.push("/menu")}>View menu</PrimaryButton>
-                </div>
-                <div className={styles.images}>
+                </motion.div>
+                <motion.div className={styles.images} variants={variants}>
                     <div className={styles.image1}></div>
                     <div className={styles.image2}></div>
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
         </Container>
     )
+}
+
+const variants= {
+    initial: { opacity: 0, transform: "translateY(-50px)" },
+    animate: { opacity: 1, transform: "translateY(0px)",  }
 }
